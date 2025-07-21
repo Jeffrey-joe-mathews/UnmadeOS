fb66f0d3e5d76c8dea35ec1406bf4cc6b774ef57
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-07-21 12:17:33 +0530

    made the BIOS print a message

--- Makefile
+++ Makefile
@@ -8,5 +8,6 @@ $(BUILD)/main.bin : $(SRC)/main.asm
 	@mkdir -p $(BUILD)										# we silence the output by using @ because it is repetitive
 	$(ASM) -f bin $(SRC)/main.asm -o $(BUILD)/main.bin
 
+$(BUILD)/main.img : $(BUILD)/main.bin
 	cp $(BUILD)/main.bin $(BUILD)/main.img
 	truncate -s 1440k $(BUILD)/main.img
--- src/main.asm
+++ src/main.asm
@@ -1,15 +1,47 @@
 ; Upon booting, a computer first enters the BIOS(Basic Input Output System)
 ; what a bios does is, it seraches throught the memory of bootable debices and loads it into the memory of 0x7c00 which is the Origin(ORG)
-;   and then checks for this signature 0xAA55 to verify if it is a bootable image.
+;   and then the bootloader at 0x700c checks for thie signature 0xAA55 to verify if it is a bootable image.
 
 BITS 16                     ; Tells the assembler to generate 16-bit code; required for real mode execution
 ORG  0x7c00                 ; sets the origin at the address... so that all operation start from that address
                             ; Tells the assembler the code will be loaded at that partivular address
 main:
+    XOR ax, ax              ; sets ax to 0
+    MOV ds, ax              ; assigns the data segment space to 0 (for loading data)
+    MOV es, ax              ; assigns the etra segment to zero (for doing operations like MOV)
+    MOV ss, ax              ; assigns stack segment to zero (for stack operations)
+
+    MOV sp, 0x7b00          ; assigns the stack pointer to an arbitrary address away from the memory (stack memory offset)
+    MOV si, os_boot_msg     ; initializes the address of the os_boot_msg
+    CALL print              
     HLT
 
 halt:
     JMP halt 
+
+print:
+    PUSH si                 ; saves all the three register values just in case for future use
+    PUSH ax
+    PUSH bx
+
+print_loop:
+    LODSB                   ; LOaDs a String Byte into al from the address si and increments si to point to the next character of the string
+    OR al,al                ; triggers a zero flag(ZF) if and only if al is zero
+    JZ print_end            ; Jump if string end has been reached
+
+    MOV ah, 0x0e            ; BIOS teletype function code for printing onto the BIOS
+    MOV bh, 0               ; sets the page number. useful when working with many monitors or pages
+    INT 0x10                ; sets a BIOS video interrupt
+
+    JMP print_loop          ; loops
+
+print_end: 
+    POP bx                  ; retrieve the threee datas back to its original registers
+    POP ax
+    POP si
+    RET
+
+os_boot_msg: DB "Unmade OS :: initializing -> Hollow shell",10,0
 
 TIMES 510-($-$$) DB 0       ; the bootloader must be exactly 512 bytes... 510 for code and data, 2 for signature
                             ; $$ is the address at the start of the file and $ is the current address
